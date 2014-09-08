f8d78b1ff4343ce8e318fa1fa6b95f29a39729cf
lucasmontec <lucas_carvalhaes@hotmail.com>
2014-09-08 20:06:53 -0300

    Adicioner codigo de teste para o lexer e para o analisador sintático.

--- CC2-Compilador-LA-C/src/main/Main.java
+++ CC2-Compilador-LA-C/src/main/Main.java
@@ -6,12 +6,29 @@
 
 package main;
 
+import antlr.capirotoLexer;
+import antlr.capirotoParser;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+
 /**
- *
- * @author joaovitor
+ *  Classe principal de teste de gramática.
+ * 
+ * @author joaovitor & Lucas M Carvalhaes
  */
 public class Main {
     public static void main(String ...args){
+        //Stream de texto
+        ANTLRInputStream inputStream = new ANTLRInputStream("helloworld");
+        //Lexer para gerar os tokens
+        capirotoLexer lexer = new capirotoLexer(inputStream);
+        //Estrutura comum de stream de tokens
+        CommonTokenStream stream = new CommonTokenStream(lexer);
+        //Parser do programa
+        capirotoParser parser = new capirotoParser(stream);
         
+        //Chama o token inicial
+        System.out.println(parser.helloworld().toStringTree());
     }
 }
