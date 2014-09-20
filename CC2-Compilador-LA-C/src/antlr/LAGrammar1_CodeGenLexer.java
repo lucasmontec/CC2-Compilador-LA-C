c34725aa92d821b99b016d09a920e8e7bdad2b93
joaovitor <jvbrandaom@gmail.com>
2014-09-20 18:56:51 -0300

    Merge origin/master
    Conflicts solved:
    	CC2-Compilador-LA-C/src/CodeGeneration/Generator.java
    	CC2-Compilador-LA-C/src/antlr/LAGrammar1_CodeGenParser.java
    	CC2-Compilador-LA-C/src/main/Main.java

--- CC2-Compilador-LA-C/src/antlr/LAGrammar1_CodeGenLexer.java
+++ CC2-Compilador-LA-C/src/antlr/LAGrammar1_CodeGenLexer.java
@@ -1,4 +1,4 @@
-// Generated from C:\Users\Lucas\git\CC2-Compilador-LA-C\CC2-Compilador-LA-C\src\grammars\LAGrammar1_CodeGen.g4 by ANTLR 4.1
+// Generated from /home/joaovitor/NetBeansProjects/CC2-Compilador-LA-C/CC2-Compilador-LA-C/src/grammars/LAGrammar1_CodeGen.g4 by ANTLR 4.1
 package antlr;
 
   import Semantic.TokenSymbol;
@@ -90,14 +90,14 @@ public class LAGrammar1_CodeGenLexer extends Lexer {
 		case 66: WSNL_action((RuleContext)_localctx, actionIndex); break;
 		}
 	}
-	private void WSNL_action(RuleContext _localctx, int actionIndex) {
-		switch (actionIndex) {
-		case 1: _channel = HIDDEN;  break;
-		}
-	}
 	private void COMMENT_action(RuleContext _localctx, int actionIndex) {
 		switch (actionIndex) {
 		case 0: _channel = HIDDEN;  break;
+		}
+	}
+	private void WSNL_action(RuleContext _localctx, int actionIndex) {
+		switch (actionIndex) {
+		case 1: _channel = HIDDEN;  break;
 		}
 	}
 
